5c4d7a6728f1277f3de6b8ed90334e8a3d69656c
Vladislav <vladislav.lifirenko@gmail.com>
2022-12-29 15:02:34 +0200

    Terrain Meshes (WIP)

---
+++ Assets/Scripts/Model/PerlinOctave.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PTG.Model
+{
+    [Serializable]
+    public class PerlinOctave
+    {
+        public float amplitude = 1f;
+        public float frequency = 1f;
+    }
+}
